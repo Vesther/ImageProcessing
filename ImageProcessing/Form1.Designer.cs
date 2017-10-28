4cffc5b5e1b82f49123ccafd991438f9ff72a01e
Kevin Nowald <nicokevi2210@yahoo.de>
2017-10-28 15:53:52 +0200

    Initial Commit

---
+++ ImageProcessing/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ImageProcessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(50, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(845, 482);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(151, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Apply filter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(24, 5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(338, 529);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(246, 29);
+            this.panel1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(257, 532);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Load image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(590, 532);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Save image";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 580);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "ImageProcessing";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ImageProcessing/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageProcessing
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Add our filter selection strings to the ComboBox element
+            comboBox1.Items.AddRange(new Object[] { "Blur", "Sharpen", "Edges", "Emboss" });
+            // And preselect the first one (blur)
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private Bitmap convolute(Bitmap img)
+        {
+            // 3x3 matrices defining the image processing kernel that will be convoluted with our bitmap
+            float[,] blur = new float[3, 3] {
+                { 0.109634f, 0.111842f, 0.109634f },
+                { 0.111842f, 0.114094f, 0.111842f },
+                { 0.109634f, 0.111842f, 0.109634f }
+            };
+
+            float[,] edge = new float[3, 3] {
+                { -1, -1, -1 },
+                { -1, 8, -1 },
+                { -1, -1, -1 }
+            };
+
+            float[,] sharpen = new float[3, 3] {
+                { 0, -1, 0 },
+                { -1, 5, -1 },
+                { 0, -1, 0 }
+            };
+
+            float[,] emboss = new float[3, 3] {
+                { -2, -1, 0 },
+                { -1, 1, 1 },
+                { 0, 1, 2 }
+            };
+
+            int selected = comboBox1.SelectedIndex;
+
+            float[,] matrix;
+
+            switch (selected)
+            {
+                case 0:
+                    matrix = blur;
+                    break;
+                case 1:
+                    matrix = sharpen;
+                    break;
+                case 2:
+                    matrix = edge;
+                    break;
+                case 3:
+                    matrix = emboss;
+                    break;
+                default:
+                    matrix = blur;
+                    break;
+            }
+
+            Bitmap result = new Bitmap(img.Width, img.Height);
+
+            for (int i = 1; i < img.Width-1; i++)
+            {
+                for (int j = 1; j < img.Height-1; j++)
+                {
+                    Color color = img.GetPixel(i, j);
+                    float red = 0;
+                    float green = 0;
+                    float blue = 0;
+                    // float alpha = 0;
+
+                    for (int ii = 0; ii < 3; ii++)
+                    {
+                        for (int jj = 0; jj < 3; jj++)
+                        {
+                            Color temp = img.GetPixel(i - 1 + ii, j - 1 + jj);
+                            red += temp.R * matrix[ii, jj];
+                            green += temp.G * matrix[ii, jj];
+                            blue += temp.B * matrix[ii, jj];
+                            // alpha += temp.A * matrix[ii, jj];
+                        }
+                    }
+
+                    if (red > 255) red = 255;
+                    if (blue > 255) blue = 255;
+                    if (green > 255) green = 255;
+                    // if (alpha > 255) alpha = 255;
+
+
+                    if (red < 0) red = 0;
+                    if (blue < 0) blue = 0;
+                    if (green < 0) green = 0;
+                    // if (alpha < 0) alpha = 0;
+
+                    result.SetPixel(i, j, Color.FromArgb((int)red, (int)green, (int)blue));
+                }
+            }
+
+            return result;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Guard against unloaded image
+            if (pictureBox1.Image == null) return;
+
+            PictureBox pb1 = pictureBox1;
+            Bitmap bmp = new Bitmap(pb1.Image);
+            pb1.Image = convolute(bmp);
+            pb1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            string path;
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Image files (*.jpg, *.png)|*.jpg; *.png";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                path = file.FileName;
+                pictureBox1.Image = Image.FromFile(path);
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            // Guard against unloaded image
+            if (pictureBox1.Image == null) return;
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Jpeg Image|*.jpg|PNG Image|*.png";
+            saveFileDialog1.Title = "Save an Image File";
+            saveFileDialog1.FileName = "test";
+            saveFileDialog1.ShowDialog();
+
+            // Guard against empty filenames
+            if (saveFileDialog1.FileName == "") return;
+
+            System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+
+            switch (saveFileDialog1.FilterIndex)
+            {
+                case 1:
+                    // JPEG
+                    pictureBox1.Image.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    break;
+                case 2:
+                    // PNG
+                    pictureBox1.Image.Save(fs, System.Drawing.Imaging.ImageFormat.Png);
+                    break;
+            }
+
+            fs.Close(); 
+        }
+    }
+}
